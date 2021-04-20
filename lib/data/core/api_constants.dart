6ea2fcb842c050e0ed49955d0b78c111982a4aa8
siddarth-bhura <sidjain.sb@gmail.com>
2021-04-20 12:16:49 +0530

    First Push

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._(); //This means it is a private construvtor and cannot be initiated outside.
+
+  static const String BASE_URL = "";
+  static const String API_KEY = "";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
